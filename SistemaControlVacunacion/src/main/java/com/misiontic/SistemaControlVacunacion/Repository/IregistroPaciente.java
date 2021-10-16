99b87fe3a735deff32aaa69017729e62dec3130c
juanestebanalarcon <=>
2021-10-16 12:41:19 -0500

    segunda carpeta eliminada

--- SistemaControlVacunacion/src/main/java/com/misiontic/SistemaControlVacunacion/Repository/IregistroPaciente.java
+++
@@ -1,9 +0,0 @@
-package com.misiontic.SistemaControlVacunacion.Repository;
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.misiontic.SistemaControlVacunacion.Model.registroPacienteModel;
-public interface IregistroPaciente extends JpaRepository<registroPacienteModel,String>{
-Optional<registroPacienteModel> findBynoDocumento(String noDocumento);
-
-}
