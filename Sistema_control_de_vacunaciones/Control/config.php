5a858a419ee678bd62f07341d058a4461d6ea819
juanestebanalarcon <=>
2021-10-16 12:40:19 -0500

    Se eliminarán ambas carpetas para subilas nuevamente

--- Sistema_control_de_vacunaciones/Control/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-///Define parameters to connect to the database
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS","sqlPass11");
-define("DB_NAME","controlVac");
- 
-
-?>
